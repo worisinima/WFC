d3083e1ee1c19904dda607aaa1610ee707b0df39
YivanLee <649628455@qq.com>
2021-04-19 13:39:38 +0800

    FreeImage库

--- WFC/Simple3DRendering.cpp
+++ WFC/Simple3DRendering.cpp
@@ -17,6 +17,9 @@
 #include <math.h>
 #include <stdlib.h>
 
+#include "FreeImage.h"
+#pragma comment(lib, "FreeImage.lib")
+
 using namespace std;
 
 namespace FileHelper
@@ -87,23 +90,23 @@ public:
 };
 
 template<class Type>
-class FVector2D
+class vec2
 {
 public:
-	FVector2D(Type x, Type y) : X(x), Y(y) {}
-	FVector2D() :X(0), Y(0) {}
-	~FVector2D() {}
-	FVector2D operator+ (const FVector2D& Ref) { FVector2D ret; ret.X = this->X + Ref.X; ret.Y = this->Y + Ref.Y; return ret; }
-	FVector2D operator- (const FVector2D& Ref) { FVector2D ret; ret.X = this->X - Ref.X; ret.Y = this->Y - Ref.Y; return ret; }
-	FVector2D operator+ (const FVector2D& Ref)const { FVector2D ret; ret.X = this->X + Ref.X; ret.Y = this->Y + Ref.Y; return ret; }
-	FVector2D operator- (const FVector2D& Ref)const { FVector2D ret; ret.X = this->X - Ref.X; ret.Y = this->Y - Ref.Y; return ret; }
-	FVector2D operator* (const FVector2D& Ref) { FVector2D ret; ret.X = this->X * Ref.X; ret.Y = this->Y * Ref.Y; return ret; }
-	FVector2D operator* (const Type& fval)const { FVector2D ret; ret.X = this->X * fval; ret.Y = this->Y * fval; return ret; }
-	FVector2D operator/ (const FVector2D& Ref) { FVector2D ret; ret.X = this->X / Ref.X; ret.Y = this->Y / Ref.Y; return ret; }
-	FVector2D operator/ (const Type& fval) { FVector2D ret; ret.X = this->X / fval; ret.Y = this->Y / fval; return ret; }
-	void operator= (const FVector2D& Ref) { this->X = Ref.X; this->Y = Ref.Y; }
-	bool operator== (const FVector2D& Ref)const { return this->X == Ref.X && this->Y == Ref.Y; }
-	bool operator!= (const FVector2D& Ref)const { return this->X != Ref.X || this->Y != Ref.Y; }
+	vec2(Type x, Type y) : X(x), Y(y) {}
+	vec2() :X(0), Y(0) {}
+	~vec2() {}
+	vec2 operator+ (const vec2& Ref) { vec2 ret; ret.X = this->X + Ref.X; ret.Y = this->Y + Ref.Y; return ret; }
+	vec2 operator- (const vec2& Ref) { vec2 ret; ret.X = this->X - Ref.X; ret.Y = this->Y - Ref.Y; return ret; }
+	vec2 operator+ (const vec2& Ref)const { vec2 ret; ret.X = this->X + Ref.X; ret.Y = this->Y + Ref.Y; return ret; }
+	vec2 operator- (const vec2& Ref)const { vec2 ret; ret.X = this->X - Ref.X; ret.Y = this->Y - Ref.Y; return ret; }
+	vec2 operator* (const vec2& Ref) { vec2 ret; ret.X = this->X * Ref.X; ret.Y = this->Y * Ref.Y; return ret; }
+	vec2 operator* (const Type& fval)const { vec2 ret; ret.X = this->X * fval; ret.Y = this->Y * fval; return ret; }
+	vec2 operator/ (const vec2& Ref) { vec2 ret; ret.X = this->X / Ref.X; ret.Y = this->Y / Ref.Y; return ret; }
+	vec2 operator/ (const Type& fval) { vec2 ret; ret.X = this->X / fval; ret.Y = this->Y / fval; return ret; }
+	void operator= (const vec2& Ref) { this->X = Ref.X; this->Y = Ref.Y; }
+	bool operator== (const vec2& Ref)const { return this->X == Ref.X && this->Y == Ref.Y; }
+	bool operator!= (const vec2& Ref)const { return this->X != Ref.X || this->Y != Ref.Y; }
 	void Print() { cout << "X: " << X << "  " << "Y: " << Y << endl; }
 	Type X, Y;
 };
@@ -111,60 +114,71 @@ public:
 class vec3
 {
 public:
-	vec3(){e[0] = 0; e[1] = 0; e[2] = 0;}
-	vec3(double e0, double e1, double e2) { e[0] = e0; e[1] = e1; e[2] = e2; }
-	double x() const { return e[0]; } double y() const { return e[1]; } double z() const { return e[2]; }
-	double r() const { return e[0]; } double g() const { return e[1]; } double b() const { return e[2]; }
+	vec3(){X = 0; Y = 0; Z = 0;}
+	vec3(const double& e0, const double& e1, const double& e2) { X = e0; Y = e1; Z = e2; }
 	const vec3& operator+() const { return *this; }
-	vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
-	double operator[](int i) const { return e[i]; }
-	double& operator[](int i) { return e[i]; }
-	vec3& operator+=(const vec3& v) { e[0] += v.e[0]; e[1] += v.e[1]; e[2] += v.e[2]; return *this; }
-	vec3& operator*=(const vec3& v) { e[0] *= v.e[0]; e[1] *= v.e[1]; e[2] *= v.e[2]; return *this; }
-	vec3& operator/=(const vec3& v) { e[0] /= v.e[0]; e[1] /= v.e[1]; e[2] /= v.e[2]; return *this; }
-	vec3& operator-=(const vec3& v) { e[0] -= v.e[0]; e[1] -= v.e[1]; e[2] -= v.e[2]; return *this; }
-	vec3& operator*=(const double t) { e[0] *= t; e[1] *= t; e[2] *= t; return *this; }
-	vec3& operator/=(const double t) { double k = 1.0 / t; e[0] *= k; e[1] *= k; e[2] *= k; return *this; }
-	double length() const { return sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]); }
-	double squared_length() const { return e[0] * e[0] + e[1] * e[1] + e[2] * e[2]; }
-	void make_unit_vector() { double k = 1.0 / sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]); e[0] *= k; e[1] *= k; e[2] *= k; }
-	double e[3];
+	vec3 operator-() const { return vec3(-X, -Y, -Z); }
+	vec3& operator+=(const vec3& v) { X += v.X; Y += v.Y; Z += v.Z; return *this; }
+	vec3& operator*=(const vec3& v) { X *= v.X; Y *= v.Y; Z *= v.Z; return *this; }
+	vec3& operator/=(const vec3& v) { X /= v.X; Y /= v.Y; Z /= v.Z; return *this; }
+	vec3& operator-=(const vec3& v) { X -= v.X; Y -= v.Y; Z -= v.Z; return *this; }
+	vec3& operator*=(const double& t) { X *= t; Y *= t; Z *= t; return *this; }
+	vec3& operator/=(const double& t) { double k = 1.0 / t; X *= k; Y *= k; Z *= k; return *this; }
+	double length() const { return sqrt(X * X + Y * Y + Z * Z); }
+	double squared_length() const { return X * X + Y * Y + Z * Z; }
+	void make_unit_vector() { double k = 1.0 / sqrt(X * X + Y * Y + Z * Z); X *= k; Y *= k; Z *= k; }
+	void Print(){ cout << "( " << X << ", " << Y << ", " << Z << " )" << endl; }
+	double X, Y, Z;
 };
-std::istream& operator >> (std::istream& is, vec3& t) {is >> t.e[0] >> t.e[1] >> t.e[2];return is;}
-std::ostream& operator<<(std::ostream& os, const vec3& t) {os << t.e[0] << " " << t.e[1] << " " << t.e[2];return os;}
-vec3 operator+(const vec3& v1, const vec3& v2) {return vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2]);}
-vec3 operator-(const vec3& v1, const vec3& v2) {return vec3(v1.e[0] - v2.e[0], v1.e[1] - v2.e[1], v1.e[2] - v2.e[2]);}
-vec3 operator*(const vec3& v1, const vec3& v2) {return vec3(v1.e[0] * v2.e[0], v1.e[1] * v2.e[1], v1.e[2] * v2.e[2]);}
-vec3 operator/(const vec3& v1, const vec3& v2) {return vec3(v1.e[0] / v2.e[0], v1.e[1] / v2.e[1], v1.e[2] / v2.e[2]);}
-vec3 operator*(double t, const vec3& v) {return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);}
-vec3 operator/(vec3 v, double t) {return vec3(v.e[0] / t, v.e[1] / t, v.e[2] / t);}
-vec3 operator*(const vec3& v, double t) {return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);}
-double dot(const vec3& v1, const vec3& v2) {return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[1] + v1.e[2] * v2.e[2];}
-vec3 cross(const vec3& v1, const vec3& v2) {return vec3((v1.e[1] * v2.e[2] - v1.e[2] * v2.e[1]),(-(v1.e[0] * v2.e[2] - v1.e[2] * v2.e[0])),(v1.e[0] * v2.e[1] - v1.e[1] * v2.e[0]));}
+std::istream& operator >> (std::istream& is, vec3& t) {is >> t.X >> t.Y >> t.Z;return is;}
+std::ostream& operator<<(std::ostream& os, const vec3& t) {os << t.X << " " << t.Y << " " << t.Z;return os;}
+vec3 operator+(const vec3& v1, const vec3& v2) {return vec3(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);}
+vec3 operator-(const vec3& v1, const vec3& v2) {return vec3(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);}
+vec3 operator*(const vec3& v1, const vec3& v2) {return vec3(v1.X * v2.X, v1.Y * v2.Y, v1.Z * v2.Z);}
+vec3 operator/(const vec3& v1, const vec3& v2) {return vec3(v1.X / v2.X, v1.Y / v2.Y, v1.Z / v2.Z);}
+vec3 operator*(double t, const vec3& v) {return vec3(t * v.X, t * v.Y, t * v.Z);}
+vec3 operator/(vec3 v, double t) {return vec3(v.X / t, v.Y / t, v.Z / t);}
+vec3 operator*(const vec3& v, const double& t) {return vec3(t * v.X, t * v.Y, t * v.Z);}
+double dot(const vec3& v1, const vec3& v2) {return v1.X * v2.X + v1.Y * v2.Y + v1.Z * v2.Z;}
+vec3 cross(const vec3& v1, const vec3& v2) {return vec3((v1.Y * v2.Z - v1.Z * v2.Y),(-(v1.X * v2.Z - v1.Z * v2.X)),(v1.X * v2.Y - v1.Y * v2.X));}
 vec3 normalize(vec3 v) {return v / v.length();}
 
-class Float3X3
+class float3x3
 {
-	Float3X3() {e[0] = 0; e[1] = 0; e[2] = 0;e[3] = 0; e[4] = 0; e[5] = 0;e[6] = 0; e[7] = 0; e[8] = 0;}
-	Float3X3(const float& v_00, const float& v_01, const float& v_02,const float& v_10, const float& v_11, const float& v_12,const float& v_20, const float& v_21, const float& v_22)
+public:
+	float3x3() { e00 = e01 = e02 = e10 = e11 = e12 = e20 = e21 = e22 = 0; }
+	float3x3(const float& v_00, const float& v_01, const float& v_02, const float& v_10, const float& v_11, const float& v_12, const float& v_20, const float& v_21, const float& v_22)
 	{
-		e[0] = v_00; e[1] = v_01; e[2] = v_02;
-		e[3] = v_10; e[4] = v_11; e[5] = v_12;
-		e[6] = v_20; e[7] = v_21; e[8] = v_22;
+		e00 = v_00; e01 = v_01; e02 = v_02;
+		e10 = v_10; e11 = v_11; e12 = v_12;
+		e20 = v_20; e21 = v_21; e22 = v_22;
 	}
-	float e[9];
+	float e00; float e01; float e02; 
+	float e10; float e11; float e12; 
+	float e20; float e21; float e22;
+	float3x3 Inverse()
+	{
+		float3x3 invMatrix;
+		invMatrix.e00 = e00; invMatrix.e01 = e10; invMatrix.e02 = e20;
+		invMatrix.e10 = e01; invMatrix.e11 = e11; invMatrix.e12 = e21;
+		invMatrix.e20 = e02; invMatrix.e21 = e12; invMatrix.e22 = e22;
+		return invMatrix;
+	}
 };
-
-vec3 mul(const Float3X3& matrix, const vec3& vec)
+vec3 mul(const float3x3& matrix, const vec3& vec)
 {
 	vec3 Ret;
-
+	Ret.X = matrix.e00 * vec.X + matrix.e01 * vec.Y + matrix.e02 * vec.Z;
+	Ret.Y = matrix.e10 * vec.X + matrix.e11 * vec.Y + matrix.e12 * vec.Z;
+	Ret.Z = matrix.e20 * vec.X + matrix.e21 * vec.Y + matrix.e22 * vec.Z;
 	return Ret;
 }
-vec3 mul(const vec3& vec, const Float3X3& matrix)
+vec3 mul(const vec3& vec, const float3x3& matrix)
 {
 	vec3 Ret;
-
+	Ret.X = matrix.e00 * vec.X + matrix.e10 * vec.Y + matrix.e20 * vec.Z;
+	Ret.Y = matrix.e01 * vec.X + matrix.e11 * vec.Y + matrix.e21 * vec.Z;
+	Ret.Z = matrix.e02 * vec.X + matrix.e12 * vec.Y + matrix.e22 * vec.Z;
 	return Ret;
 }
 
@@ -172,18 +186,13 @@ class FImage
 {
 public:
 	//创建默认是黑色填充
-	FImage(int X, int Y, string ImageName) :
-		SizeX(X),
-		SizeY(Y),
-		ImageName(ImageName)
+	FImage(const int& X, const int& Y, string ImageName) :SizeX(X), SizeY(Y), ImageName(ImageName)
 	{
-		for (int i = 0; i < SizeX; i++)
-		{
-			vector<FColor> row(SizeY);
-			Imagedata.push_back(row);
-		}
+		Imagedata = new vector<FColor>();
+		for (int i = 0; i < SizeX * SizeY; i++) 
+			Imagedata->push_back(FColor(0));
 	}
-	bool SaveImageToDesk()
+	bool SaveImageAsPPMToDesk()
 	{
 		const string& Path = FileHelper::GetDesktopPath();
 		cout << endl << "begin save image to desk operation" << endl;
@@ -194,7 +203,7 @@ public:
 		{
 			for (int x = 0; x < SizeX; x++)
 			{
-				FColor& color = Imagedata[x][y];
+				FColor& color = (*Imagedata)[y * SizeX + x];
 				fout << color.R << " " << color.G << " " << color.B << "\n";
 			}
 		}
@@ -202,39 +211,69 @@ public:
 		cout << endl << "Save image successfully" << endl;
 		return bCreateImage;
 	}
-	void ClearImage(FColor& ClearColor)
+	void SaveImageAsBMPToDesk()
+	{
+		FreeImage_Initialise();
+
+		FIBITMAP* bitmap = FreeImage_Allocate(SizeX, SizeY, BPP);
+		for (int y = 0; y < SizeY; y++)
+		{
+			for (int x = 0; x < SizeX; x++)
+			{
+				RGBQUAD val;
+				val.rgbRed = (*Imagedata)[y * SizeX + x].R;
+				val.rgbGreen = (*Imagedata)[y * SizeX + x].G;
+				val.rgbBlue = (*Imagedata)[y * SizeX + x].B;
+				FreeImage_SetPixelColor(bitmap, x, y, &val);
+			}
+		}
+		const string& Path = FileHelper::GetDesktopPath();
+		FreeImage_Save(FIF_BMP, bitmap, (Path + "/" + ImageName + ".bmp").c_str());
+		FreeImage_DeInitialise();
+	}
+	void ClearImage(const FColor& ClearColor)
 	{
 		cout << endl << "Begin clear image operation" << endl;
-		int nx = SizeX; int ny = SizeY;
-		for (int i = 0; i < nx; i++)
+		for (int y = 0; y < SizeY; y++)
 		{
-			for (int j = 0; j < ny; j++)
+			for (int x = 0; x < SizeX; x++)
 			{
-				Imagedata[i][j] = ClearColor;
+				(*Imagedata)[y * SizeX + x] = ClearColor;
 			}
 		}
 	}
-	void SetPixleColor(const FColor& newData, const FVector2D<int>& PixleLocation)
+	void SetPixleColor(const FColor& newData, const vec2<int>& PixleLocation)
 	{
-		if (PixleLocation.X < SizeX && PixleLocation.Y < SizeY) Imagedata[PixleLocation.X][PixleLocation.Y] = newData;
+		if (PixleLocation.X < SizeX && PixleLocation.Y < SizeY) (*Imagedata)[PixleLocation.Y * SizeX + PixleLocation.X] = newData;
 	}
-	void SetPixleColor(const int& newData, const FVector2D<int>& PixleLocation)
+	void SetPixleColor(const int& newData, const vec2<int>& PixleLocation)
 	{
-		if (PixleLocation.X < SizeX && PixleLocation.Y < SizeY) Imagedata[PixleLocation.X][PixleLocation.Y] = FColor(newData);
+		if (PixleLocation.X < SizeX && PixleLocation.Y < SizeY) (*Imagedata)[PixleLocation.Y * SizeX + PixleLocation.X] = FColor(newData);
 	}
 
 	int SizeX, SizeY;
 	string ImageName, ImagePath;
-	vector<vector<FColor>> Imagedata;
+	vector<FColor>* Imagedata;
+	int BPP = 24;// Since we’re outputting three 8bit RGB values
 };
 
 //如果要生成就打开这里
 #if 1
 int main()
 {
-	vec3 a = vec3(1, 0, 0);
-	vec3 b = vec3(0, 1, 0);
-	cout << cross(a, b) << endl;
+	FImage* outImage = new FImage(64, 64, "OutImage");
+	for (unsigned int y = 0; y < outImage->SizeY; y++)
+	{
+		for (unsigned int x = 0; x < outImage->SizeX; x++)
+		{
+			FColor OutColor;
+			vec2<float> uv = vec2<float>((float)x / (float)(outImage->SizeX), (float)y / (float)(outImage->SizeY));
+
+			OutColor = FColor(uv.X * 255, uv.Y * 255, 0);
+			outImage->SetPixleColor(OutColor, vec2<int>(x, y));
+		}
+	}
+	outImage->SaveImageAsBMPToDesk();
 
 	return 0;
 }
